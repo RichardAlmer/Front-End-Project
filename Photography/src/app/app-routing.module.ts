091e50c034aa46b067fabb0870778b5105ad60a7
Mario Hartleb <mhartleb83@yahoo.com>
2021-03-29 13:29:12 +0200

    create routing

--- Photography/src/app/app-routing.module.ts
+++ Photography/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent} from './landing-page/landing-page.component';
+import { AboutPageComponent} from './about-page/about-page.component';
+import { ContactPageComponent} from './contact-page/contact-page.component';
+import { CustomersPageComponent} from './customers-page/customers-page.component';
+import { PortfolioPageComponent} from './portfolio-page/portfolio-page.component';
+import { TestimonialPageComponent} from './testimonial-page/testimonial-page.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {
+    path:"",component: LandingPageComponent
+  },
+  {
+    path:"about",component: AboutPageComponent
+  },
+  {
+    path:"contact",component: ContactPageComponent
+  },
+  {
+    path:"customers",component: CustomersPageComponent
+  },
+  {
+    path:"portfolio",component: PortfolioPageComponent
+  },
+  {
+    path:"testimonial",component: TestimonialPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
