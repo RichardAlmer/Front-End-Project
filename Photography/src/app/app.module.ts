9aa04b12a0490b3147c7cdcfaa509802e5aa6437
Mathias Fuchs <mathias_fuchs_87@yahoo.de>
2021-03-29 11:10:42 +0200

    Made Skeleton and components

---
+++ Photography/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { CustomersPageComponent } from './customers-page/customers-page.component';
+import { TestimonialPageComponent } from './testimonial-page/testimonial-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LandingPageComponent,
+    PortfolioPageComponent,
+    AboutPageComponent,
+    FooterComponent,
+    ContactPageComponent,
+    CustomersPageComponent,
+    TestimonialPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
