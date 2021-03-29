3e5cc870449b7c6829a262e4b2319456d12ef850
Richard Kumhofer <richard.kumhofer@gmail.com>
2021-03-29 13:13:04 +0200

    changes

--- Photography/src/app/contact-page/contact-page.component.ts
+++ Photography/src/app/contact-page/contact-page.component.ts
@@ -10,20 +10,26 @@ export class ContactPageComponent implements OnInit {
   contact = new FormGroup({
     firstName: new FormControl("", Validators.required),
     lastName:new FormControl("", Validators.required),
-    email: new FormControl("", [Validators.required, Validators.email]),
+    email: new FormControl("", Validators.required),
     massage: new FormControl("", Validators.required),
     date: new FormControl("", Validators.required)
   })
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
+    var sendAlert = document.getElementById('sendAlert');
+
     if (this.contact.valid) {
-      // alert(`Thank you for booking at TravelGavel.\nTo pay ${this.cartService.getTotal().toFixed(2)} EUR.\nWe will send you the invoice by email.`)
+      sendAlert.innerHTML = 'Your message has been sent.';
+      sendAlert.style.color = 'green';
       this.contact.reset();
+    }else{
+      sendAlert.innerHTML = 'Please fill in all fields.';
+      sendAlert.style.color = 'red';
     }
   }
 
