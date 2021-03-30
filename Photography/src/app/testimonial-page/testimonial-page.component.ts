70b95c5b544a82fc12b3d71f62398d8f11413784
Mathias Fuchs <mathias_fuchs_87@yahoo.de>
2021-03-30 14:37:58 +0200

    Webcam Testing

--- Photography/src/app/testimonial-page/testimonial-page.component.ts
+++ Photography/src/app/testimonial-page/testimonial-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from '../header.service';
 import { NavbarService } from '../navbar.service';
+import { WebcamImage } from 'ngx-webcam';
+import { Subject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-testimonial-page',
@@ -14,6 +16,20 @@ export class TestimonialPageComponent implements OnInit {
   ngOnInit(): void {
     this.nav.show();
     this.head.show();
+  }
+
+  public webcamImage: WebcamImage = null;
+  private trigger: Subject<void> = new Subject<void>();
+  triggerSnapshot(): void {
+    this.trigger.next();
+  }
+  handleImage(webcamImage: WebcamImage): void {
+    console.info('Saved webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
   }
 
 }
