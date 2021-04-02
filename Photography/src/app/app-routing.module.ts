be3922f6e8bd5e22867e16e0a8987a913d02d2a6
Mathias Fuchs <mathias_fuchs_87@yahoo.de>
2021-04-02 09:22:25 +0200

    deactivated documentation page and link

--- Photography/src/app/app-routing.module.ts
+++ Photography/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent} from './landing-page/landing-page.component';
-import { AboutPageComponent} from './about-page/about-page.component';
-import { ContactPageComponent} from './contact-page/contact-page.component';
-import { CustomersPageComponent} from './customers-page/customers-page.component';
-import { PortfolioPageComponent} from './portfolio-page/portfolio-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { CustomersPageComponent } from './customers-page/customers-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { DocumentationComponent } from './documentation/documentation.component';
@@ -16,33 +16,33 @@ import { OfferPageComponent } from './offer-page/offer-page.component';
 
 const routes: Routes = [
   {
-    path:"",component: LandingPageComponent
+    path: "", component: LandingPageComponent
   },
   {
-    path:"about",component: AboutPageComponent
+    path: "about", component: AboutPageComponent
   },
   {
-    path:"contact",component: ContactPageComponent
+    path: "contact", component: ContactPageComponent
   },
   {
-    path:"customers",component: CustomersPageComponent
+    path: "customers", component: CustomersPageComponent
   },
   {
-    path:"portfolio",component: PortfolioPageComponent
+    path: "portfolio", component: PortfolioPageComponent
   },
   {
-    path:"privacyPolicy", component: PrivacyPolicyComponent
+    path: "privacyPolicy", component: PrivacyPolicyComponent
   },
   {
     path: "imprint", component: ImprintComponent
   },
-  {
-    path: "documentation", component: DocumentationComponent
-  },
+  // {
+  //   path: "documentation", component: DocumentationComponent
+  // },
   {
     path: "offers", component: OfferPageComponent
   },
-  
+
 ];
 
 @NgModule({
